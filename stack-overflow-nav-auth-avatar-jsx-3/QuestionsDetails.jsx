d0b8119e9bfc7cb71b8ce9ac3be78d0a891458cf
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-25 10:33:38 +0530

    Update QuestionsDetails.jsx

--- stack-overflow-nav-auth-avatar-jsx-3/QuestionsDetails.jsx
+++ stack-overflow-nav-auth-avatar-jsx-3/QuestionsDetails.jsx
@@ -1,7 +1,90 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
+import "./Question.css";
 const QuestionsDetails = () => {
-  return  (
- <div> </div>
-  );
+  const id = useParams()
+ var questionsList = [{ 
+         _id: '1',
+         upVotes: 3,
+         downVotes: 2,
+         noOfAnswers: 2,
+         questionTitle: "What is a function?",
+         questionBody: "It meant to be",
+         questionTags: ["java", "node js", "react js", "mongo db", "express js"],
+         userPosted: "mano",
+         userId: 1,
+         askedOn: "jan 1",
+       answer: [{
+             answerBody: "Answer",
+           userAnswered: 'kumar',
+             answeredOn: "jan 2",
+             userId: 2,
+         }]
+     },{ 
+         _id: '2',
+         upVotes: 3,
+         downVotes: 2,
+      noOfAnswers: 0,
+         questionTitle: "What is a function?",
+         questionBody: "It meant to be",
+         questionTags: ["javascript", "R", "python"],
+         userPosted: "mano",
+         askedOn: "jan 1",
+         userId: 1,
+         answer: [{
+             answerBody: "Answer",
+             userAnswered: 'kumar',
+          answeredOn: "jan 2",
+             userId: 2,
+         }]
+     },{ 
+         _id: '3',
+         upVotes: 3,
+        downVotes: 2,
+        noOfAnswers: 0,
+         questionTitle: "What is a function?",
+        questionBody: "It meant to be",
+         questionTags: ["javascript", "R", "python"],
+         userPosted: "mano",
+         askedOn: "jan 1",
+        userId: 1,
+         answer: [{
+             answerBody: "Answer",
+             userAnswered: 'kumar',
+            answeredOn: "jan 2",
+             userId: 2,
+         }]
+     }]
+     return (
+       <div className = "question-details-page">
+         {
+           
+           questionsList === null ? 
+           <h1> Loading...</h1>
+           {
+             questionsList.filter(question => question._id === id ).map(question => (
+               <div key = {question._id}>
+                 <section className = "question-details-container">
+                   <h1>{question.questionTitle}</h1>
+                   <div className = "question-details-container-2">
+                     <img src = "ThumbUpIcon" alt = "" width = "18"/>
+                     <p>{question.upVotes - question.downVotes}</p>
+                     <img src = "ThumbDownAltIcon" alt = "" width = "18"/>
+                   <p className = "question-body"></p>
+                   </section>
+
+               </div>
+             ))
+
+           }
+        
+          }
+
+
+       </div>
+            
+     );
 };
 export default QuestionsDetails;
